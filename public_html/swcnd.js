92e81b269ba9ea12b20c037ae3e5726e8f616e43
Jorge Air <fraxito@gmail>
2022-10-26 23:55:39 +0200

    Añadido el instalador automático en móvil

---
+++ public_html/swcnd.js
@@ -0,0 +1,38 @@
+let deferredPrompt;
+const version = "1.0";
+const cacheName = 'iSonidos-${version}';
+
+self.addEventListener('beforeinstallprompt', (e) => {
+  // Prevent Chrome 67 and earlier from automatically showing the prompt
+  e.preventDefault();
+  // Stash the event so it can be triggered later.
+  deferredPrompt = e;
+});
+
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(async function() {
+    try {
+      return await fetch(event.request);
+    } catch (err) {
+      return caches.match(event.request);
+    }
+  }());
+});
+
+
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(cacheName).then(cache => {
+      return cache.addAll([
+        '/',
+        '/index.html'
+      ])
+          .then(() => self.skipWaiting());
+    })
+  );
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(self.clients.claim());
+});
